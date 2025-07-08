ac36d861e85fb6c6ece7a95614f11596bd65c567
baptistekieffer <baptiste67.k@gmail.com>
2025-07-08 10:48:31 +0200

    feat: Enhance QuestionCard to track correct answers and navigate after quiz completion

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -2,17 +2,30 @@
 
 import {useState} from "react";
 import {QuestionWithShuffledAnswers} from "@/types/question";
+import {redirect} from "next/navigation";
 
 export default function QuestionCard(props: { questions: QuestionWithShuffledAnswers[] }) {
-    const [questionNumber, setQuestionNumber] = useState<number>(0);
+    const [currentQuestionNb, setCurrentQuestionNb] = useState<number>(0);
+    const [nbCorrectAnswers, setNbCorrectAnswers] = useState<number>(0);
     const questions = props.questions;
+
+    const handlePlayerResponds = (isCorrect: boolean) => {
+        if (isCorrect) {
+            setNbCorrectAnswers(nbCorrectAnswers + 1);
+        }
+        if (currentQuestionNb + 1 < questions.length) {
+            setCurrentQuestionNb(currentQuestionNb + 1);
+        } else {
+            redirect("..");
+        }
+    }
 
     return (
         <div className="flex flex-col bg-neutral-900 rounded-xl p-10 gap-10">
-            <h1 className="text-xl">{questions[questionNumber].question}</h1>
+            <h1 className="text-xl">{questions[currentQuestionNb].question}</h1>
             <div className="flex flex-col gap-3">
-                { questions[questionNumber].answers.map((answer, index) => (
-                    <button key={index} className="text-lg p-3 rounded-xl bg-black cursor-pointer border border-transparent hover:border-white"> { answer.text } </button>
+                { questions[currentQuestionNb].answers.map((answer, index) => (
+                    <button key={index} onClick={() => { handlePlayerResponds(answer.isCorrect) }} className="text-lg p-3 rounded-xl bg-black cursor-pointer border border-transparent hover:border-white"> { answer.text } </button>
                 ))}
             </div>
         </div>
