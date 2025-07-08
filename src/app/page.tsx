e3c86efab9e634ddc562a166854ff6b2304a5f2b
baptistekieffer <baptiste67.k@gmail.com>
2025-07-09 00:25:19 +0200

    feat(ui): Update styles for global layout and quiz components with gradients and improved spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 
 export default async function Home() {
     return (
-        <div className="flex flex-col gap-5 px-10">
+        <div className="flex flex-col gap-15 px-10">
             <div className="flex flex-col items-center">
-                <h1 className="py-15 text-4xl font-bold text-center">QUIZ FC</h1>
-                <Link href="/quizs" className="py-5 px-10 text-2xl bg-neutral-900 rounded-xl border border-transparent hover:border-white">Tous les quizs</Link>
+                <h1 className="py-15 text-5xl font-bold text-center">QUIZ FC</h1>
+                <Link href="/quizs" className="py-5 px-10 text-2xl bg-gradient-to-br from-stone-800 to-stone-950 rounded-xl border border-transparent hover:border-white">Tous les quizs</Link>
             </div>
             <QuizList/>
         </div>
--- src/components/QuizCard.tsx
+++ src/components/QuizCard.tsx
@@ -10,10 +10,15 @@ export default function QuizCard( props: { quiz: Quiz } ) {
     }
 
     return (
-        <div className="flex flex-col bg-neutral-900 rounded-xl p-10 gap-2">
-            <h1 className="text-2xl font-bold"> {quiz.name}</h1>
-            <em>Difficulty : {quiz.difficulty}</em>
-            <Link href={`/quizs/${quiz.id}`} className="p-2 mt-5 bg-black border border-transparent hover:border-white rounded-xl text-center"> Démarrer le quiz </Link>
+        <div className="flex flex-col bg-gradient-to-br bg-stone-800 to-stone-950 text-white rounded-lg p-10 gap-5 shadow-lg hover:shadow-2xl transition-shadow">
+            <h1 className="text-2xl font-bold text-center">{quiz.name}</h1>
+            <em className="text-center text-lg">Difficulté : {quiz.difficulty}</em>
+            <Link
+                href={`/quizs/${quiz.id}`}
+                className="p-3 mt-5 bg-green-800 border border-transparent hover:bg-green-700 rounded-lg text-center  hover:border-white transition-all"
+            >
+                Démarrer le quiz
+            </Link>
         </div>
-    )
+    );
 }
