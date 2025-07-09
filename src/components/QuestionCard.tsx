9bff5cea6e699f05c8222405ece81d82604958ca
baptistekieffer <baptiste67.k@gmail.com>
2025-07-09 14:50:03 +0200

    feat(ui): add response feedback and delay for question navigation in QuestionCard

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -7,17 +7,24 @@ import {redirect, usePathname} from "next/navigation";
 export default function QuestionCard(props: { questions: QuestionWithShuffledAnswers[] }) {
     const [currentQuestionNb, setCurrentQuestionNb] = useState<number>(0);
     const [nbCorrectAnswers, setNbCorrectAnswers] = useState<number>(0);
+    const [hasResponded, setHasResponded] = useState<boolean>(false);
+    const [responseIndex, setResponseIndex] = useState<number>();
     const questions = props.questions;
     const pathname = usePathname();
 
-    const handlePlayerResponds = (isCorrect: boolean) => {
+    const handlePlayerResponds = (isCorrect: boolean, index: number) => {
         const updateNbCorrectAnswers = nbCorrectAnswers + (isCorrect ? 1 : 0)
-        if (currentQuestionNb + 1 < questions.length) {
-            setNbCorrectAnswers(updateNbCorrectAnswers);
-            setCurrentQuestionNb(currentQuestionNb + 1);
-        } else {
-            redirect(pathname + `/result?score=${updateNbCorrectAnswers}&total=${questions.length}`);
-        }
+        setHasResponded(true);
+        setResponseIndex(index);
+        setTimeout(() => {
+            if (currentQuestionNb + 1 < questions.length) {
+                setNbCorrectAnswers(updateNbCorrectAnswers);
+                setCurrentQuestionNb(currentQuestionNb + 1);
+                setHasResponded(false);
+            } else {
+                redirect(pathname + `/result?score=${updateNbCorrectAnswers}&total=${questions.length}`);
+            }
+        }, 1000)
     }
 
     return (
@@ -26,7 +33,9 @@ export default function QuestionCard(props: { questions: QuestionWithShuffledAns
             <h1 className="text-xl">{questions[currentQuestionNb].question}</h1>
             <div className="flex flex-col gap-3">
                 { questions[currentQuestionNb].answers.map((answer, index) => (
-                    <button key={index} onClick={() => { handlePlayerResponds(answer.isCorrect) }} className="text-lg p-3 rounded-lg text-black bg-gray-200  hover:bg-gray-300 cursor-pointer border border-transparent hover:border-white"> { answer.text } </button>
+                    <button key={index} onClick={() => { handlePlayerResponds(answer.isCorrect, index) }}
+                        className={`text-lg p-2 rounded-lg text-black bg-gray-200  hover:bg-gray-300  cursor-pointer border-5  ${hasResponded ? answer.isCorrect ? "border-green-500" : index == responseIndex ? "border-red-500" : "border-transparent" : "border-transparent"}` }>
+                        { answer.text } </button>
                 ))}
             </div>
         </div>
